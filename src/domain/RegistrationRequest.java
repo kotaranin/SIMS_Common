516b3521fbb1d879445b56724a9f644300fd585a
kotaranin <kotaraninuros@gmail.com>
2025-09-01 17:31:59 +0200

    Registration SO implemented. Operations enum updated accordingly.

--- src/domain/RegistrationRequest.java
+++ src/domain/RegistrationRequest.java
@@ -4,21 +4,18 @@
  */
 package domain;
 
-import java.io.Serializable;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  *
- * @author Uros
+ * @author kotar
  */
-public class StudentOfficer implements Serializable, AbstractDO {
+public class RegistrationRequest implements AbstractDO {
 
-    private Long idStudentOfficer;
+    private Long idRegistrationRequest;
     private String firstName;
     private String lastName;
     private String email;
@@ -26,11 +23,11 @@ public class StudentOfficer implements Serializable, AbstractDO {
     private boolean admin;
     private StudyLevel studyLevel;
 
-    public StudentOfficer() {
+    public RegistrationRequest() {
     }
 
-    public StudentOfficer(Long idStudentOfficer, String firstName, String lastName, String email, String password, boolean admin, StudyLevel studyLevel) {
-        this.idStudentOfficer = idStudentOfficer;
+    public RegistrationRequest(Long idRegistrationRequest, String firstName, String lastName, String email, String password, boolean admin, StudyLevel studyLevel) {
+        this.idRegistrationRequest = idRegistrationRequest;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -39,12 +36,12 @@ public class StudentOfficer implements Serializable, AbstractDO {
         this.studyLevel = studyLevel;
     }
 
-    public Long getIdStudentOfficer() {
-        return idStudentOfficer;
+    public Long getIdRegistrationRequest() {
+        return idRegistrationRequest;
     }
 
-    public void setIdStudentOfficer(Long idStudentOfficer) {
-        this.idStudentOfficer = idStudentOfficer;
+    public void setIdRegistrationRequest(Long idRegistrationRequest) {
+        this.idRegistrationRequest = idRegistrationRequest;
     }
 
     public String getFirstName() {
@@ -97,63 +94,42 @@ public class StudentOfficer implements Serializable, AbstractDO {
 
     @Override
     public String toString() {
-        return firstName + " " + lastName;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 71 * hash + Objects.hashCode(this.email);
-        hash = 71 * hash + Objects.hashCode(this.password);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final StudentOfficer other = (StudentOfficer) obj;
-        if (!Objects.equals(this.email, other.email)) {
-            return false;
-        }
-        return Objects.equals(this.password, other.password);
+        return firstName;
     }
 
     @Override
     public String getTable() {
-        return "student_officer";
+        return "registration_request";
     }
 
     @Override
     public List<AbstractDO> getList(ResultSet resultSet) throws Exception {
-        List<AbstractDO> studentOfficers = new LinkedList<>();
+        List<AbstractDO> registrationRequests = new LinkedList<>();
         while (resultSet.next()) {
-            StudentOfficer studentOfficer = new StudentOfficer();
-            studentOfficer.setIdStudentOfficer(resultSet.getLong(getTable() + ".id_student_officer"));
-            studentOfficer.setFirstName(resultSet.getString(getTable() + ".first_name"));
-            studentOfficer.setLastName(resultSet.getString(getTable() + ".last_name"));
-            studentOfficer.setEmail(resultSet.getString(getTable() + ".email"));
-            studentOfficer.setPassword(resultSet.getString(getTable() + ".password"));
-            studentOfficer.setAdmin(resultSet.getBoolean(getTable() + ".admin"));
+            RegistrationRequest registrationRequest = new RegistrationRequest();
+            registrationRequest.setIdRegistrationRequest(resultSet.getLong(getTable() + ".id_registration_request"));
+            registrationRequest.setFirstName(resultSet.getString(getTable() + ".first_name"));
+            registrationRequest.setLastName(resultSet.getString(getTable() + ".last_name"));
+            registrationRequest.setEmail(resultSet.getString(getTable() + ".email"));
+            registrationRequest.setPassword(resultSet.getString(getTable() + ".password"));
+            registrationRequest.setAdmin(resultSet.getBoolean(getTable() + ".admin"));
             StudyLevel studyLevel = new StudyLevel();
             studyLevel.setIdStudyLevel(resultSet.getLong(studyLevel.getTable() + ".id_study_level"));
             studyLevel.setName(resultSet.getString(studyLevel.getTable() + ".name"));
-            studentOfficer.setStudyLevel(studyLevel);
-            studentOfficers.add(studentOfficer);
+            registrationRequest.setStudyLevel(studyLevel);
+            registrationRequests.add(registrationRequest);
         }
-        return studentOfficers;
+        return registrationRequests;
     }
 
     @Override
     public String getPrimaryKey() {
-        return getTable() + ".id_student_officer = " + idStudentOfficer;
+        return "id_registration_request = " + idRegistrationRequest;
+    }
+
+    @Override
+    public String getInsertColumns() {
+        return "first_name, last_name, email, password, admin, id_study_level";
     }
 
     @Override
@@ -167,7 +143,7 @@ public class StudentOfficer implements Serializable, AbstractDO {
     }
 
     @Override
-    public void prepareStatement(PreparedStatement preparedStatement) throws SQLException {
+    public void prepareStatement(PreparedStatement preparedStatement) throws Exception {
         preparedStatement.setString(1, firstName);
         preparedStatement.setString(2, lastName);
         preparedStatement.setString(3, email);
@@ -175,10 +151,4 @@ public class StudentOfficer implements Serializable, AbstractDO {
         preparedStatement.setBoolean(5, admin);
         preparedStatement.setLong(6, studyLevel.getIdStudyLevel());
     }
-
-    @Override
-    public String getInsertColumns() {
-        return "first_name, last_name, email, password, admin, id_study_level";
-    }
-
 }
